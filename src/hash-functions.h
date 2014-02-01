823d45ba0c102e7f71a713c83ccf079430033b68
vivek <vivekgoswami10@gmail.com>
2014-02-01 12:28:09 -0800

    minor fix

--- src/hash-functions.h
+++ src/hash-functions.h
@@ -28,6 +28,7 @@
 //typedef uint32_t (*hash_function)(uint8_t*, uint16_t len);
 
 static uint32_t BloomFilterHashFn(void *data, uint16_t datalen, uint8_t iter, uint32_t hash_size);
+uint32_t JHash  (uint8_t* str, uint16_t len);
 uint32_t RSHash  (uint8_t* str, uint16_t len);
 uint32_t JSHash  (uint8_t* str, uint16_t len);
 uint32_t PJWHash (uint8_t* str, uint16_t len);
@@ -36,7 +37,6 @@ uint32_t BKDRHash(uint8_t* str, uint16_t len);
 uint32_t SDBMHash(uint8_t* str, uint16_t len);
 uint32_t DJBHash (uint8_t* str, uint16_t len);
 uint32_t DEKHash (uint8_t* str, uint16_t len);
-uint32_t BPHash  (uint8_t* str, uint16_t len);
 uint32_t FNVHash (uint8_t* str, uint16_t len);
 
 
